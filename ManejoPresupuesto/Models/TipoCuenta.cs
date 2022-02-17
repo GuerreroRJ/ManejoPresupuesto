ad9d7c8ec1f55955d6193c52eb959b13eb80798a
Rolando <rolandojgr09@gmail.com>
2022-02-17 18:41:30 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,42 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta //: IValidatableObject //SE IMPLEMENTO LA INTERFAZ IVALIDATABLE
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")] //{0} se obtiene el nombre de la variable
+        [PrimeraLetraMayuscula]
+        //[StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1} ")]
+        //[Display(Name = "Nombre del tipo cuenta")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        //Validacion con interfaz
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0) { 
+        //        var primeraLetra = Nombre[0].ToString();
+        //        if (primeraLetra != primeraLetra.ToUpper()) {
+        //            yield return new ValidationResult("La primera letra debe ser mayuscula", 
+        //                new[] { nameof(Nombre) } );
+        //        }
+        //    }
+        //}
+
+        /**Pruebas de otras validaciones por defecto**/
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //[EmailAddress(ErrorMessage = "El campo debe ser un correo electronico válido")]
+        //public string Email { get; set; }
+        //[Range(minimum: 18, maximum: 130, ErrorMessage = "El valor de {0} debe estar entre {1} y {2}")]
+        //public int Edad { get; set; }
+        //[Url(ErrorMessage ="El campo debe ser una URL valida")]
+        //public string URL { get; set; }
+        //[CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        //[Display(Name ="Tarjeta de Crédito")]
+        //public string TarjetaDeCredito { get; set; }
+
+    }
+}
---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        void Crear(TipoCuenta tipoCuenta);
+    }
+
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        public readonly string connectionSring;
+        public RepositorioTiposCuentas(IConfiguration configuration) 
+        {
+            connectionSring = configuration.GetConnectionString("DefaultConnection");
+
+        }
+
+        public void Crear(TipoCuenta tipoCuenta) 
+        {
+            using var connection = new SqlConnection(connectionSring);
+            var id = connection.QuerySingle<int>($@"INSERT INTO TiposCuentas (Nombre,UsuarioId, Orden) Values(@Nombre, @UsuarioId, 0);
+                            SELECT SCOPE_IDENTITY();", tipoCuenta);
+            tipoCuenta.Id = id;
+        }
+    }
+}
